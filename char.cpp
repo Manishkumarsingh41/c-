52a25bbbe59f55f6fed2a7e6e937a7a864479db2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-13 21:22:25 +0530

    Add files via upload

---
+++ char.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char grade = 'A';
+    cout<<"grade is: "<<grade<<endl;
+    cout<<"size of character  is : "<< sizeof("grade")<<endl;
+    return 0;
+}
