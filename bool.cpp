d6bad1fd25b273e6870544d43a67d92fa7552097
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-13 21:24:01 +0530

    Add files via upload

---
+++ bool.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool isSafe = true;
+    cout<<isSafe<<endl;
+    cout<<"size of Boolean  is : "<< sizeof("isSafe")<<endl;
+    return 0;
+}
