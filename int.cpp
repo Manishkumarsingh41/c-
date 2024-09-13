9cfc5d67ab81e693c9196fda7d4c1cd8a0f67592
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-13 21:22:05 +0530

    Add files via upload

---
+++ int.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age =25;
+    cout<<"Age is: "<<age<<endl;
+    cout<<"size of integer is : "<< sizeof("age")<<endl;
+
+    return 0;
+}
