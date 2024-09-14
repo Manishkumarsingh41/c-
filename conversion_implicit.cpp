66a50a485b88f753a46e4175dc8a07bb60dab6ec
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-14 11:21:41 +0530

    Add files via upload

---
+++ conversion_implicit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char grade = 'A';
+    int value = grade;
+    float PI = value;
+    bool isSafe =PI;
+    cout<<"Value of A is "<<grade<<endl;
+    cout<<"Value of A is "<<value<<endl;
+    cout<<"Value of A is "<<PI<<endl;
+    cout<<"Value of A is "<<isSafe<<endl;
+    return 0;
+}
