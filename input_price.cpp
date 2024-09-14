8feb5bb57bf9f6dbc8c1f267b0b8668fce4ed2a9
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-14 11:22:37 +0530

    Add files via upload

---
+++ input_price.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int price;
+    cout<<"Enter the price:";
+    cin>> price;
+    cout<<"Your Total price is : "<<price<<endl;
+    return 0;
+}
