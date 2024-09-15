fe411ad03c79665f6e9c69e904f69f4a39d49ccd
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-15 13:10:33 +0530

    Add files via upload

---
+++ divide.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 11 ;
+    float b=2;
+    cout<<"Divide 11/2= "<<(a/b)<<endl;
+}
+   //We can also write like this by using type casting
+
+    //     cout<<"Divide 11/2 = "<<(11/double(2))<<endl;
+    // }
