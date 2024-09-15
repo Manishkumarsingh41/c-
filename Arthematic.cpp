0c5ac55dbc936dfaedc483b58b8a7bddd20ea8de
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-15 13:10:19 +0530

    Add files via upload

---
+++ Arthematic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 11 ,b=2;
+    cout<<"Sum = "<<(a+b)<<endl;
+    cout<<"Difference = "<<(a-b)<<endl;
+    cout<<"Divide = "<<(a/b)<<endl;
+    cout<<"Module = "<<(a%b)<<endl;
+    cout<<"Multiply = "<<(a*b)<<endl;
+    
+
+}
