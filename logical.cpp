e596271d2b8b79ed3ae675271db22e9b26133914
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-15 13:10:52 +0530

    Add files via upload

---
+++ logical.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //We use and (&&)
+    cout<<((3>1)&&(3>4))<<endl; //True -->1 & false -->0
+
+     //We use and (OR)
+    cout<<((3>1)||(3>4))<<endl; //True -->1 & false -->0
+
+    //We use and (!)
+    cout<<(!(3>1))<<endl; //True -->1 & false -->0
+
+return 0;
+}
