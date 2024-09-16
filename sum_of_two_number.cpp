0c3d321f7c19ada35528304b155d435a6a37ca3b
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:06:32 +0530

    Add files via upload

---
+++ sum_of_two_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b ,sum;
+    cout<<"Enter the Value of a: ";
+    cin>> a;
+    cout<<"Enter the Value of b: ";
+    cin>> b;
+    cout<<"The Sum of a and b is : "<<(a+b)<<endl;
+    return 0;
+}
