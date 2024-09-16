106839a7d926c750752d29a352f805fa727976d3
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:06:20 +0530

    Add files via upload

---
+++ Multiply_of_two_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b ,Multiply;
+    cout<<"Enter the Value of a: ";
+    cin>> a;
+    cout<<"Enter the Value of b: ";
+    cin>> b;
+    cout<<"The Multiply of a and b is : "<<(a*b)<<endl;
+    return 0;
+}
