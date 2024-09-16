975225654d9c0e618c4002da489bc4f257472f1f
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:05:44 +0530

    Add files via upload

---
+++ Difference_of_two_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b ,Difference;
+    cout<<"Enter the Value of a: ";
+    cin>> a;
+    cout<<"Enter the Value of b: ";
+    cin>> b;
+    cout<<"The Difference of a and b is : "<<(a-b)<<endl;
+    return 0;
+}
