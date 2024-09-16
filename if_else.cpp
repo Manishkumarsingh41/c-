c7469e663cd9b3cc022b0bfa55b77090946bb2af
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:44:29 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n =-25;
+
+    if(n>=0){
+        cout<<"n is positive\n"<<endl;
+
+    }else{
+        cout<<"n is negative\n"<<endl;
+    
+    return 0;
+    }
+}
