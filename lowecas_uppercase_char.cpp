c59915be4042ceb203af7bbf65dd5da4c400bf7f
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:44:46 +0530

    Add files via upload

---
+++ lowecas_uppercase_char.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+cout<<"Enter the Character"<<endl;
+cin>>ch;
+    if(ch>='a'&& ch<='z'){
+        cout<<"It is lowercase\n"<<endl;
+
+    }else{
+        cout<<"It is uppercase\n"<<endl;
+    
+    return 0;
+    }
+}
