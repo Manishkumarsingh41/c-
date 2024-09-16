012f1c5de23202aa221e9df25031fe89829663c2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:45:29 +0530

    Add files via upload

---
+++ odd_even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter the integer :"<<endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"It is even "<<endl;
+
+    }else{
+        cout<<"It is odd "<<endl;
+    
+    return 0;
+    }
+}
