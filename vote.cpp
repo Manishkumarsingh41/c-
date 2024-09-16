e9266bc94cc3e0fd2839835b1dcb03aa05fd6b46
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:45:55 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter your age :"<<endl;
+    cin>>n;
+    if(n>=18){
+        cout<<"Congratulation you are eligible for voting\n"<<endl;
+
+    }else{
+        cout<<"You are not eligible for voting\n"<<endl;
+    
+    return 0;
+    }
+}
