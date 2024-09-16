722429e38588b61c0c8e3c18f51f8b662cc5c4df
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-16 17:06:09 +0530

    Add files via upload

---
+++ Module_of_two_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b ,Module;
+    cout<<"Enter the Value of a: ";
+    cin>> a;
+    cout<<"Enter the Value of b: ";
+    cin>> b;
+    cout<<"The Module of a and b is : "<<(a%b)<<endl;
+    return 0;
+}
