489d70a662a68541aca1de5b7fadce4bb28e1f11
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-09-17 17:48:18 +0530

    Add files via upload

---
+++ infinite_while_loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,count =1;
+    while(count<=n){
+        cout<<count<<" ";
+        count++;
+    }
+    cout<<endl;
+    return 0;
+}
